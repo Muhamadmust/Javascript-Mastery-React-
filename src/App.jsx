d1dc3624fd3d5168a99bc7cf21cb7281e0b1b9cc
Muhammad Mustapha <muhadmustapha5104@gmail.com>
2026-07-30 09:54:34 +0100

    Add logos and search bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,28 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from './assets/vite.svg'
-// import heroImg from './assets/hero.png'
-import './App.css'
-import {useEffect, useState} from "react";
+import { useState } from 'react'
+import Search from "./components/Search.jsx";
 
-const Card = ({ title, content}) => {
-    const [count, setCount] = useState(0)
-    const [hasLiked, setHasLiked] = useState(false);
 
-    useEffect(() => {
-        console.log(`${title} has been liked: ${hasLiked}`)
-    },[hasLiked]);
-
-    return (
-    <div className="card" onClick={() => setCount(count + 1)}>
-      <h2>{title} <br/> {count || null}</h2>
-      <p>{content}</p>
-        <button onClick={() => setHasLiked(!hasLiked)}>
-            {hasLiked ? '😍🤍' : '❤️'}
-        </button>
-    </div>
-  )
-}
 
 const App = () => {
-  return (
-    <div className="card-container">
-      <Card title="Star Wars" content="Action" rating={5} isCool={true}  />
-      <Card title="Star Trek" content="Adventure"/>
-      <Card title="Lion King" content="Cartoon"/>
 
-    </div>
-  )
+    const [searchTerm, setSearchTerm] = useState('');
+
+
+    return (
+        <main>
+            <div className="pattern" />
+            <div>
+                <h1 className="wrapper">
+                    <header>
+                        <img src={"./hero.png"} alt="Hero Banner" />
+                        <h1>Find <span className="text-gradient">Movies</span> You'll Enjoy Without the Hassle</h1>
+                    </header>
+
+                    <Search serchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
+                    <h1>{searchTerm}</h1>
+                </h1>
+            </div>
+        </main>
+    )
 }
 export default App
