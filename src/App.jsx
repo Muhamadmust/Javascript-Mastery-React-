19340f0da6aa06b193f3fc84cd15b1d963561e28
Muhammad Mustapha <muhadmustapha5104@gmail.com>
2026-08-05 16:00:15 +0100

    feat: conected the API

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,65 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Search from "./components/Search.jsx";
 
+// API: Application Programing interface
 
+const API_BASE_URL = 'https://api.themoviedb.org/3'
+
+const API_KEY = import.meta.env.VITE_TMBD_API_KEY;
+
+const API_OPTIONS = {
+    method: 'GET',
+    headers: {
+        'Accept': 'application/json',
+        Authorization: `Bearer ${API_KEY}`
+    }
+}
 
 const App = () => {
 
     const [searchTerm, setSearchTerm] = useState('');
 
+    const [errorMessage, setErrorMessage] = useState('');
+
+    const fetchMovies = async () => {
+        try {
+            const endpoint = `${API_BASE_URL}/dicover/ movie?sort_by=popularity.desc`
+
+            const response = await fetch(endpoint, API_OPTIONS)
+
+            if (!response.ok) {
+                throw new Error('Failed to fetch movies.');
+            }
+
+            const data = await response.json();
+            console.log(data)
+        } catch (error) {
+            console.log(`Error Fetching Movies ${error}`);
+            setErrorMessage('Error Fetching Movies. Please try again later.');
+        }
+    }
+
+    useEffect(() => {
+        fetchMovies();
+    }, []);
 
     return (
         <main>
             <div className="pattern" />
-            <div>
-                <h1 className="wrapper">
-                    <header>
-                        <img src={"./hero.png"} alt="Hero Banner" />
-                        <h1>Find <span className="text-gradient">Movies</span> You'll Enjoy Without the Hassle</h1>
-                    </header>
 
-                    <Search serchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
-                    <h1>{searchTerm}</h1>
-                </h1>
+            <div className="wrapper">
+                <header>
+                    <img src={"./hero.png"} alt="Hero Banner" />
+                    <h1>Find <span className="text-gradient">Movies</span> You'll Enjoy Without the Hassle</h1>
+
+                <Search serchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
+                </header>
+
+                <section className="all-movies">
+                    <h1>All Movies</h1>
+
+                    {errorMessage && <p className="text-red-500">{errorMessage}</p>}
+                </section>
             </div>
         </main>
     )
